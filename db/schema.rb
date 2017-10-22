316a2c5e26e741ddb9ba6d3334e7c2ad4ebcdd10
Alibek <ashiralialibek@gmail.com>
2017-10-22 11:30:37 +0000

    second commit

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160830215558) do
 
-  create_table "bets", force: :cascade do |t|
-    t.integer  "bet"
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "bids", force: :cascade do |t|
     t.integer  "price"
     t.datetime "start_date"
